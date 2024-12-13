cb88c7a1b6f415f74687a95515ef0d160fa65e7a
Andrei Burvel <burvelandrei@gmail.com>
2024-12-13 17:22:58 +0300

    init

--- online_store_drf/settings.py
+++ online_store_drf/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'cart',
+    'cart_product',
+    'product',
 ]
 
 MIDDLEWARE = [
---
+++ product/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def discounted_price(self):
+        """Возвращает цену с учетом скидки."""
+        return self.price * (1 - self.discount / 100)
