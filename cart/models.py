0ef03b1cbd902971223e57f0bdb4cb7675696ba7
Andrei Burvel <burvelandrei@gmail.com>
2024-12-18 14:13:52 +0300

    V 1.1 модели для cart и cart_product

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from .models import CustomUser
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateField(auto_now_add=True)
+    expires_at = models.DateField()
+
+    @property
+    def is_active(self):
+        return self.expires_at > timezone.now()
+
+    @property
+    def total_price(self):
+        """Считает общую стоимость корзины."""
+        return sum(item.total_price for item in self.cart_products.all())
--- cart_product/models.py
+++ cart_product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from .models import Cart, Product
 
-# Create your models here.
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_products')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name} in {self.cart}"
+
+    @property
+    def total_price(self):
+        """Считает общую стоимость продукта в корзине с учетом скидки."""
+        return self.product.discounted_price * self.quantity
