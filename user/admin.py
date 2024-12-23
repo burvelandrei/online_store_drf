63160c579ce950fa5235bc69fa928adfe231a295
Andrei Burvel <burvelandrei@gmail.com>
2024-12-23 15:13:21 +0300

    V 1.13 админка для юзеров

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "role",
+        "email",
+    )
+
+    search_fields = ("username",)
+    ordering = ("username",)
+
+admin.site.register(User, UserAdmin)
